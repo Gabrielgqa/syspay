d850cc59381c5c02973b468c84a6fe0ba21f912b
GabrielGQA <gabrielgqa@gmail.com>
2020-01-09 11:08:00 -0300

    lista usuários (teste)

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const user = [
+    { name: 'Admin', email: 'admin@adm.com' },
+  ];
+  res.status(200).json(user);
+});
+
 module.exports = app;
